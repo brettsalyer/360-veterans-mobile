84e5323ac7b964a3ef9e638cb9a9057f3e4dd24c
Brett Salyer <brettsalyer@gmail.com>
2021-03-22 19:15:36 -0400

    Added a template page for the user account

---
+++ lib/screens/account_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+
+/*
+Author: Brett Salyer
+Date: 03-22-2021
+Desc: The template for the account page
+
+*/
+
+class AccountPage extends StatefulWidget {
+  @override
+  _AccountPageState createState() => _AccountPageState();
+}
+
+class _AccountPageState extends State<AccountPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Account Page"),
+    );
+  }
+}
