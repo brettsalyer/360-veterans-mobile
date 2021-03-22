0a15813623eba819c332a4800710310868852ec4
Brett Salyer <brettsalyer@gmail.com>
2021-03-22 19:22:38 -0400

    Added template for home page

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+
+/*
+Author: Brett Salyer
+Date: 03-22-2021
+Desc: The template for the home page
+
+*/
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Home Page"),
+    );
+  }
+}
