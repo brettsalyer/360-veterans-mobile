c5e8cc21db62b985598670500f181eaf5542d7ce
Brett Salyer <brettsalyer@gmail.com>
2021-03-22 19:22:21 -0400

    Added template for the tracker page

---
+++ lib/screens/tracker_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+
+/*
+Author: Brett Salyer
+Date: 03-22-2021
+Desc: The template for the tracker page
+
+*/
+
+class TrackerPage extends StatefulWidget {
+  @override
+  _TrackerPageState createState() => _TrackerPageState();
+}
+
+class _TrackerPageState extends State<TrackerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Tracker Page"),
+    );
+  }
+}
